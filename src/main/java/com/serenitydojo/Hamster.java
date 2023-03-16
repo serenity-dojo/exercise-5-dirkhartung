d9dd518e4ab6395d6bae781ec4deea7ab8e8e2e4
Dirk Hartung <dirk.hartung@publicissapient.com>
2023-03-16 15:45:00 +0100

    My try on Exercise - Create an inherited class

---
+++ src/main/java/com/serenitydojo/Hamster.java
@@ -0,0 +1,20 @@
+package com.serenitydojo;
+
+public class Hamster extends Pet {
+
+    private final String favoriteToy;
+
+    public Hamster(String name, int age, String favoriteToy) {
+        super(name, age);
+        this.favoriteToy = favoriteToy;
+    }
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    @Override
+    public String play() {
+        return "runs in wheel";
+    }
+}
